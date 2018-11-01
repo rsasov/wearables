111b7b9419905af60e547772e56ed13a5827b091
rsasov <rosen.sasov@mail.com>
2018-11-01 17:12:51 +0200

    Add custom form

--- wearablesapp/views.py
+++ wearablesapp/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ActivityPeriodForm
 
 # Create your views here.
 def index(request):
     return render(request,'results.html')
+
+def activity(request):
+    if request.method == 'POST' and form.is_valid():
+    form = ActivityPeriodForm(request.POST or None)
+    #if form.is_valid():
+        # TODO: ADD GRAPHICS
+
+    #else:
+        # TODO: CREATE FORM
+    context = {
+        "form": form,
+    }
+    return render(request,'activity.html',context)
+
+def test(request):
+    return HttpResponse("Hello there")
