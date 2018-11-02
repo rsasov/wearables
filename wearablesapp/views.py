ec61ea0a2e2ab0666ecde715145a2ed97a1168ae
rsasov <rosen.sasov@mail.com>
2018-11-02 22:35:28 +0200

    rpy2 custom function call fix

--- wearablesapp/views.py
+++ wearablesapp/views.py
@@ -8,6 +8,10 @@ from rpy2.robjects import r
 def prepare_plotting_data(start,end,interval):
     r.load("~/Documents/django-rsas/wearables/.RData")
     wearableProc = importr("wearableProc", lib_loc="~/Documents/django-rsas/wearables/R/x86_64-pc-linux-gnu-library/3.4")
+    utils = importr('utils')
+    ggplot = importr('ggplot2')
+    stringr = importr("stringr", lib_loc="~/Documents/django-rsas/wearables/R/x86_64-pc-linux-gnu-library/3.4")
+
     wearableProc.prepare_interval_activity_data(start,end,interval)
 
 # Create your views here.
@@ -23,7 +27,7 @@ def activity(request):
         start = form['start_interval'].value()
         end = form['end_interval'].value()
         interval = form['time_split'].value()
-        prepare_plotting_data(start,end,interval)
+        prepare_plotting_data('start','end','interval')
 
             #start_interval = form.cleaned_data['start_interval']
             #end_interval = form.cleaned_data['end_interval']
